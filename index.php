9625898ee6b1281d100196cdf149aac76b631f4a
popwandee@gmail.com <popwandee@gmail.com>
2018-12-05 05:48:47 +0700

    change path vendor

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ use LINE\LINEBot\KitchenSink\Dependency;
 use LINE\LINEBot\KitchenSink\Route;
 use LINE\LINEBot\KitchenSink\Setting;
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 $setting = Setting::getSetting();
 $app = new \Slim\App($setting);

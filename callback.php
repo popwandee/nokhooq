5aa57e5cee3344d8f1235bc86ee8e199bf2c4b4f
popwandee@gmail.com <popwandee@gmail.com>
2018-12-05 09:31:30 +0700

    update

--- callback.php
+++ callback.php
@@ -78,19 +78,25 @@ try {
 }
 
 
+// แปลงข้อความรูปแบบ JSON  ให้อยู่ในโครงสร้างตัวแปร array
+$content = json_decode($events, true);
+$replyToken = $content['content'][0]['replyToken'];
+    $userID = $content['content'][0]['source']['userId'];
+    $sourceType = $content['content'][0]['source']['type'];
+    $is_postback = NULL;
+    $is_message = NULL;
+
 foreach ($events as $event) {
   // Postback Event
 	if (($event instanceof \LINE\LINEBot\Event\PostbackEvent)) {
     $info='Postback message has come';
 		$logger->info($info); //บันทึกว่าได้รับข้อความแล้ว
-    $bot->replyText($reply_token, $info);//ตอบผู้ใช้ว่าได้รับข้อความแล้ว
 		continue;
 	}
 	// Location Event
 	if  ($event instanceof LINE\LINEBot\Event\MessageEvent\LocationMessage) {
     $info="location -> ".$event->getLatitude().",".$event->getLongitude();
     $logger->info($info);
-    $bot->replyText($reply_token, $info);//ตอบผู้ใช้ว่าได้รับ พิกัดอะไรมา
 		continue;
 	}
 
@@ -102,180 +108,47 @@ foreach ($events as $event) {
     $explodeText=explode(" ",$text);
 
     switch ($explodeText[0]) {
-      case '#':
-      $json = '{
- "type":"bubble",
- "direction":"ltr",
- "header":{
-   "type":"box",
-   "layout":"vertical",
-   "contents":[
-     {"type":"text", "text":"header"}
-   ]
- },
- "hero":{
-   "type":"image",
-   "url":"https://example.com/hero.png"
- },
- "body":{
-   "type":"box",
-   "layout":"vertical",
-   "contents":[
-     {"type":"text", "text":"body"}
-   ]
- },
- "footer":{
-   "type":"box",
-   "layout":"vertical",
-   "contents":[
-     {"type":"text", "text":"footer"}
-   ]
- },
- "styles":{
-   "body":{
-     "separator": true,
-     "separatorColor": "#000000"
-   }
- }
-}';
-      $builder = new BubbleContainerBuilder(
-      ContainerDirection::LTR,
-      new BoxComponentBuilder(ComponentLayout::VERTICAL, [new TextComponentBuilder('header')]),
-      new ImageComponentBuilder('https://example.com/hero.png'),
-      new BoxComponentBuilder(ComponentLayout::VERTICAL, [new TextComponentBuilder('body')]),
-      new BoxComponentBuilder(ComponentLayout::VERTICAL, [new TextComponentBuilder('footer')]),
-      BubbleStylesBuilder::builder()->setBody(new BlockStyleBuilder(null, true, '#000000'))
-  );
-  $this->assertEquals(json_decode($json, true), $builder->build());
-
-      break;
-
-		  default:
-       //$handler = new ImageMessageHandler($bot, $logger, $req, $event);
-		  // $replyText=$replyMessage.$displayName.$statusMessage;
-		  break;
-            }//end switch
-      $replyText='Nothing';
-	    $bot->replyMessage($reply_token, $replyText);
+      case "p":
+                              // เรียกดูข้อมูลโพรไฟล์ของ Line user โดยส่งค่า userID ของผู้ใช้ LINE ไปดึงข้อมูล
+                              $response = $bot->getProfile($userID);
+                              if ($response->isSucceeded()) {
+                                  // ดึงค่ามาแบบเป็น JSON String โดยใช้คำสั่ง getRawBody() กรณีเป้นข้อความ text
+                                  $textReplyMessage = $response->getRawBody(); // return string
+                                  $replyData = new TextMessageBuilder($textReplyMessage);
+                                  break;
+                              }
+                              // กรณีไม่สามารถดึงข้อมูลได้ ให้แสดงสถานะ และข้อมูลแจ้ง ถ้าไม่ต้องการแจ้งก็ปิดส่วนนี้ไปก็ได้
+                              $failMessage = json_encode($response->getHTTPStatus() . ' ' . $response->getRawBody());
+                              $replyData = new TextMessageBuilder($failMessage);
+                              break;
+                          case "สวัสดี":
+                              // เรียกดูข้อมูลโพรไฟล์ของ Line user โดยส่งค่า userID ของผู้ใช้ LINE ไปดึงข้อมูล
+                              $response = $bot->getProfile($userID);
+                              if ($response->isSucceeded()) {
+                                  // ดึงค่าโดยแปลจาก JSON String .ให้อยู่ใรูปแบบโครงสร้าง ตัวแปร array
+                                  $userData = $response->getJSONDecodedBody(); // return array
+                                  // $userData['userId']
+                                  // $userData['displayName']
+                                  // $userData['pictureUrl']
+                                  // $userData['statusMessage']
+                                  $textReplyMessage = 'สวัสดีครับ คุณ '.$userData['displayName'];
+                                  $replyData = new TextMessageBuilder($textReplyMessage);
+                                  break;
+                              }
+                              // กรณีไม่สามารถดึงข้อมูลได้ ให้แสดงสถานะ และข้อมูลแจ้ง ถ้าไม่ต้องการแจ้งก็ปิดส่วนนี้ไปก็ได้
+                              $failMessage = json_encode($response->getHTTPStatus() . ' ' . $response->getRawBody());
+                              $replyData = new TextMessageBuilder($failMessage);
+                              break;
+                          default:
+                              $textReplyMessage = " คุณไม่ได้พิมพ์ ค่า ตามที่กำหนด";
+                              $replyData = new TextMessageBuilder($textReplyMessage);
+                              break;                                      
+                      }
+                      break;
+                  default:
+                      $textReplyMessage = json_encode($events);
+                      $replyData = new TextMessageBuilder($textReplyMessage);
+                      break;
+        }// end switch
     }//end if text
 }// end foreach event
-
-
-class FlexSuspectedPeople
-{
-    private static $items = [
-        '111' => [
-            'photo' => 'https://example.com/photo1.png',
-            'name' => 'Arm Chair, White',
-            'price' => 49.99,
-            'stock' => true,
-        ],
-        '112' => [
-            'photo' => 'https://example.com/photo2.png',
-            'name' => 'Metal Desk Lamp',
-            'price' => 11.99,
-            'stock' => false,
-        ],
-    ];
-    /**
-     * Create sample shopping flex message
-     *
-     * @return \LINE\LINEBot\MessageBuilder\FlexMessageBuilder
-     */
-    public static function get()
-    {
-        return FlexMessageBuilder::builder()
-            ->setAltText('Suspected People')
-            ->setContents(new CarouselContainerBuilder([
-                self::createItemBubble(111),
-                self::createItemBubble(112),
-                self::createMoreBubble()
-            ]));
-    }
-    private static function createItemBubble($itemId)
-    {
-        $item = self::$items[$itemId];
-        return BubbleContainerBuilder::builder()
-            ->setHero(self::createItemHeroBlock($item))
-            ->setBody(self::createItemBodyBlock($item))
-            ->setFooter(self::createItemFooterBlock($item));
-    }
-    private static function createItemHeroBlock($item)
-    {
-        return ImageComponentBuilder::builder()
-            ->setUrl($item['photo'])
-            ->setSize(ComponentImageSize::FULL)
-            ->setAspectRatio(ComponentImageAspectRatio::R20TO13)
-            ->setAspectMode(ComponentImageAspectMode::COVER);
-    }
-    private static function createItemBodyBlock($item)
-    {
-        $components = [];
-        $components[] = TextComponentBuilder::builder()
-            ->setText($item['name'])
-            ->setWrap(true)
-            ->setWeight(ComponentFontWeight::BOLD)
-            ->setSize(ComponentFontSize::XL);
-        $price = explode('.', number_format($item['price'], 2));
-        $components[] = BoxComponentBuilder::builder()
-            ->setLayout(ComponentLayout::BASELINE)
-            ->setContents([
-                TextComponentBuilder::builder()
-                    ->setText('$'.$price[0])
-                    ->setWrap(true)
-                    ->setWeight(ComponentFontWeight::BOLD)
-                    ->setSize(ComponentFontSize::XL)
-                    ->setFlex(0),
-                TextComponentBuilder::builder()
-                    ->setText('.'.$price[1])
-                    ->setWrap(true)
-                    ->setWeight(ComponentFontWeight::BOLD)
-                    ->setSize(ComponentFontSize::SM)
-                    ->setFlex(0)
-            ]);
-
-        return BoxComponentBuilder::builder()
-            ->setLayout(ComponentLayout::VERTICAL)
-            ->setSpacing(ComponentSpacing::SM)
-            ->setContents($components);
-    }
-    private static function createItemFooterBlock($item)
-    {
-        $color = $item['stock'] ? null : '#aaaaaa';
-        $cartButton = ButtonComponentBuilder::builder()
-            ->setStyle(ComponentButtonStyle::PRIMARY)
-            ->setColor($color)
-            ->setAction(
-                new UriTemplateActionBuilder(
-                    'Add to Cart',
-                    'https://example.com'
-                )
-            );
-        $wishButton = ButtonComponentBuilder::builder()
-            ->setAction(
-                new UriTemplateActionBuilder(
-                    'Add to wishlist',
-                    'https://example.com'
-                )
-            );
-        return BoxComponentBuilder::builder()
-            ->setLayout(ComponentLayout::VERTICAL)
-            ->setSpacing(ComponentSpacing::SM)
-            ->setContents([$cartButton, $wishButton]);
-    }
-    private static function createMoreBubble()
-    {
-        return BubbleContainerBuilder::builder()
-            ->setBody(
-                BoxComponentBuilder::builder()
-                    ->setLayout(ComponentLayout::VERTICAL)
-                    ->setSpacing(ComponentSpacing::SM)
-                    ->setContents([
-                        ButtonComponentBuilder::builder()
-                            ->setFlex(1)
-                            ->setGravity(ComponentGravity::CENTER)
-                            ->setAction(new UriTemplateActionBuilder('See more', 'https://example.com'))
-                    ])
-            );
-    }
-}//end class
